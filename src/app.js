dcaed1f265666ed3055c8c9a212f42bd4fcaf312
Sharmaharsh06 <45426197+Sharmaharsh06@users.noreply.github.com>
2018-12-12 21:19:13 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const path =require('path');
 const express=require('express');
 const app=express();
 
-app.set('views', path.join(__dirname,'views'));
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static(path.join(__dirname , 'public')));
 
 app.get('/',function(req,res){
 res.render('index',{title:"Index"})
@@ -15,4 +15,8 @@ res.render('index',{title:"Index"})
 
 app.listen(3000,()=>console.log('PS project running on port 3000'));
 
+
+function newFunction() {
+    app.set('views', path.join(__dirname, 'views'));
+}
 
